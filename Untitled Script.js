02cf1884d2830713177d6871ae1e3c803afa3bda
Aite_polygit <aite@github.com>
2023-04-18 17:41:00 +0700

    Update Untitled Script.js

--- Untitled Script.js
+++ Untitled Script.js
@@ -2,17 +2,6 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: orange; icon-glyph: magic;
 
-let d = new Date()
-let a = d.getDate().toString()
-let icloud = FileManager.iCloud()
-icloud.readImage("Lunar/Moon.png")
-
-let wLunar = new ListWidget()
-// wLunar.addText(a)
-wLunar.addImage()
-wLunar.presentLarge()
-Script.setWidget(wLunar)
-Script.complete();
 
 
 
@@ -20,66 +9,143 @@ Script.complete();
 
 
 
+/**
+*
+*  Base64 encode / decode
+*  http://www.webtoolkit.info/
+*
+eyJlcnJvciI6MCwiZGF0YSI6eyIxMzU3ODYwNzg1OTQ1NTM5MjgwIjp7ImFkcyI6W10sImJhY2t1cCI6W119fSwiZXh0Ijp7InNlcnZpbmdUb2tlbiI6Ik1Dd3hNekE0TlRBek13XHUwMDNkXHUwMDNkIn19
+**/
+await base64("decode")
 
+async function base64(type, value){
+let Base64 = {
 
+    // private property
+    _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
 
+    // public method for encoding
+    encode : function (input) {
+        var output = "";
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        var i = 0;
 
+        input = Base64._utf8_encode(input);
 
-async function loadImage(url) {
-    let request = new Request(url)
-    return request.loadImage()
+        while (i < input.length) {
+
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
+
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+
+            output = output +
+            this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+            this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+        }
+        return output;
+    },
+
+    // public method for decoding
+    decode : function (input) {
+        var output = "";
+        var chr1, chr2, chr3;
+        var enc1, enc2, enc3, enc4;
+        var i = 0;
+
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+        while (i < input.length) {
+
+            enc1 = this._keyStr.indexOf(input.charAt(i++));
+            enc2 = this._keyStr.indexOf(input.charAt(i++));
+            enc3 = this._keyStr.indexOf(input.charAt(i++));
+            enc4 = this._keyStr.indexOf(input.charAt(i++));
+
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+
+            output = output + String.fromCharCode(chr1);
+
+            if (enc3 != 64) {
+                output = output + String.fromCharCode(chr2);
+            }
+            if (enc4 != 64) {
+                output = output + String.fromCharCode(chr3);
+            }
+        }
+
+        output = Base64._utf8_decode(output);
+
+        return output;
+    },
+
+    // private method for UTF-8 encoding
+    _utf8_encode : function (string) {
+        string = string.replace(/\r\n/g,"\n");
+        var utftext = "";
+
+        for (var n = 0; n < string.length; n++) {
+
+            var c = string.charCodeAt(n);
+
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            }
+            else if((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+        }
+        return utftext;
+    },
+
+    // private method for UTF-8 decoding
+    _utf8_decode : function (utftext) {
+        var string = "";
+        var i = 0;
+        var c = c1 = c2 = 0;
+
+        while ( i < utftext.length ) {
+
+            c = utftext.charCodeAt(i);
+
+            if (c < 128) {
+                string += String.fromCharCode(c);
+                i++;
+            }
+            else if((c > 191) && (c < 224)) {
+                c2 = utftext.charCodeAt(i+1);
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                i += 2;
+            }
+            else {
+                c2 = utftext.charCodeAt(i+1);
+                c3 = utftext.charCodeAt(i+2);
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+        }
+        return string;
+    }
 }
-async function moonUp(d){
-    let canvas = new DrawContext()
-    canvas.opaque = 0
-    let a = 300, b= 300
-    canvas.size = new Size(a, b)
-    let img1 = await loadImage('https://raw.githubusercontent.com/net93/MyScriptable/main/lunar/Moon.png')
-    canvas.drawImageInRect(img1, new Rect(0, 0, a, b))
-    let path1 = new Path()
-    path1.move(new Point(a/2, 0))
-    path1.addLine(new Point(a/2, b))
-    path1.move(new Point(0, b/2))
-    path1.addLine(new Point(a, b/2))
-    path1.addEllipse(new Rect(0, 0, a, b))
-    canvas.addPath(path1)
-    canvas.strokePath()
-    let path2 = new Path()
-    d = d
-    let c = 350-d*400/15
-    path2.move(new Point(a/2, 0))
-    path2.addCurve(new Point(a/2, b), new Point(-50, 0), new Point(-50, b))
-    path2.addCurve(new Point(a/2, 0), new Point(c, b), new Point(c, 0))
-    canvas.setFillColor(new Color('000000', 0.75))
-    canvas.addPath(path2)
-    canvas.fillPath()
-    return canvas.getImage()
-}
 
-async function moonDown(d){
-    let canvas = new DrawContext()
-    canvas.opaque = 0
-    let a = 300, b= 300
-    canvas.size = new Size(a, b)
-    let img1 = await loadImage('https://raw.githubusercontent.com/net93/MyScriptable/main/lunar/Moon.png')
-    canvas.drawImageInRect(img1, new Rect(0, 0, a, b))
-    let path1 = new Path()
-    path1.move(new Point(a/2, 0))
-    path1.addLine(new Point(a/2, b))
-    path1.move(new Point(0, b/2))
-    path1.addLine(new Point(a, b/2))
-    path1.addEllipse(new Rect(0, 0, a, b))
-    canvas.addPath(path1)
-    canvas.strokePath()
-    let path2 = new Path()
-    d = (d - 15)%15
-    let c = 350-d*400/15
-    
-    path2.move(new Point(a/2, 0))
-    path2.addCurve(new Point(a/2, b), new Point(a+50, 0), new Point(a+50, b))
-    path2.addCurve(new Point(a/2, 0), new Point(c, b), new Point(c, 0))
-    canvas.setFillColor(new Color('000000', 0.75))
-    canvas.addPath(path2)
-    canvas.fillPath()
-    return canvas.getImage()
+if(type.match(/decode/i)){return console.log("1")}else {return console.log("0")}
+
 }
